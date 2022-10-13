dddfd627019f830e2847552802e0f89a6fc0424b
ElizaMedina <111034588+ElizaMedina@users.noreply.github.com>
2022-10-12 23:35:53 -0600

    Initial commit

---
+++ modelo/Estudiantes.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace modelo
+{
+    public class Estudiantes{
+        conexionDB conectar;
+
+      
+
+        private DataTable Grid_estudiante()
+        {
+            DataTable tabla = new DataTable();
+            conectar = new conexionDB();
+            conectar.AbrirConexion();
+            string consulta = string.Format("SELECT e.id_estudiante as id,e.carnet,e.nombres,e.apellidos,e.direccion,e.telefono,e.correo_electronico,t.id_tipo_sangre,e.id_tipo_sangre,e.fecha_nacimiento \r\nfrom estudiante as e inner join tipo_sangre as t on e.id_tipo_sangre = t.sangre;");
+            MySqlDataAdapter query = new MySqlDataAdapter(consulta, conectar.conectar);
+            query.Fill(tabla);
+
+            conectar.CerarConexion();
+
+            return tabla;
+        }
+       
+        public void Grid_estudiante(GridView grid)
+        {
+            grid.DataSource = Grid_estudiante();
+            grid.DataBind();
+        }
+
+    
+
+        public int crear(string carnet, string nombres, string apellidos, string direccion, string telefono, string correo_electronico, int id_tipo_sangre, string fecha_nacimiento)
+        {
+            int no_ingreso = 0;
+            conectar = new conexionDB();
+            conectar.AbrirConexion();
+            string strConsulta = string.Format("insert into estudiante(carnet,nombres,apellidos,direccion,telefono,correo_electronico,id_tipo_sangre,fecha_nacimiento) values('{0}','{1}','{2}','{3}','{4}','{5}',{6},{7});", carnet, nombres, apellidos, direccion, telefono, correo_electronico,id_tipo_sangre,fecha_nacimiento);
+            MySqlCommand insertar = new MySqlCommand(strConsulta, conectar.conectar);
+
+            insertar.Connection = conectar.conectar;
+            no_ingreso = insertar.ExecuteNonQuery();
+            conectar.CerarConexion();
+            return no_ingreso;
+
+        }
+        public int modificar(int id, string carnet, string nombres, string apellidos, string direccion, string telefono, string correo_electronico, int id_tipo_sangre, string fecha_nacimiento)
+        {
+            int no_ingreso = 0;
+            conectar = new conexionDB();
+            conectar.AbrirConexion();
+            string strConsulta = string.Format("update estudiante set carnet = '{0}',nombres = '{1}',apellidos = '{2}',direccion='{3}',telefono='{4}',correo_electronico='{5},id_tipo_sangre='{6}',fecha_nacimiento = {7} where id_estudiante = {8};", carnet, nombres, apellidos, direccion, telefono, correo_electronico, id_tipo_sangre, fecha_nacimiento, id);
+            MySqlCommand modificar = new MySqlCommand(strConsulta, conectar.conectar);
+
+            modificar.Connection = conectar.conectar;
+            no_ingreso = modificar.ExecuteNonQuery();
+            conectar.CerarConexion();
+            return no_ingreso;
+        }
+        public int eliminar(int id)
+        {
+            int no_ingreso = 0;
+            conectar = new conexionDB();
+            conectar.AbrirConexion();
+            string strConsulta = string.Format("delete from estudiante  where id_estudiante = {0};", id);
+            MySqlCommand eliminar = new MySqlCommand(strConsulta, conectar.conectar);
+
+            eliminar.Connection = conectar.conectar;
+            no_ingreso = eliminar.ExecuteNonQuery();
+            conectar.CerarConexion();
+            return no_ingreso;
+        }
+
+    }
+
+
+
+}
+}
---
+++ modelo/conexionDB.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modelo
+{
+    internal class conexionDB
+    {
+        private string cadena = "servel= localhost; database=db_medina; user=root; password=4912";
+        public MySqlConnection conectar = new MySqlConnection();
+
+
+        public void AbrirConexion()
+        {
+            try
+            {
+                conectar.ConnectionString = cadena;
+                conectar.Open();
+                // System.Diagnostics.Debug.WriteLine("Conexion Exitosa");
+
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                // Console.WriteLine(ex.StackTrace);
+
+            }
+
+        }
+        public void CerarConexion()
+        {
+
+            if (conectar.State == ConnectionState.Open)
+            {
+                conectar.Close();
+            }
+
+        }
+    }
+}
+
